1ecc5cba3e075054c3d992a957ae23bb06387c68
MOHAMED RIASKHAN <mohamedriaskhan@gmail.com>
2023-03-02 11:23:45 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using GoogleApi.Entities.Maps.Common;
+
+namespace CabInvoiceGeneratorDay30
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double flare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {Fare}");
+        }
+    }
+}
